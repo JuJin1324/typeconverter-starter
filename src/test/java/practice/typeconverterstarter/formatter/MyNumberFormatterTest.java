bb6c83e5d9d7cf26d424a0ace03fa2ecb080313d
jujin1324 <jujin1324@daum.net>
2021-12-04 19:49:26 +0900

    포맷터 - Formatter

---
+++ src/test/java/practice/typeconverterstarter/formatter/MyNumberFormatterTest.java
@@ -0,0 +1,29 @@
+package practice.typeconverterstarter.formatter;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Yoo Ju Jin(jujin1324@daum.net)
+ * Created Date : 2021/12/04
+ */
+class MyNumberFormatterTest {
+    MyNumberFormatter formatter = new MyNumberFormatter();
+
+    @Test
+    void parse() throws ParseException {
+        Number result = formatter.parse("1,000", Locale.KOREA);
+        assertThat(result).isEqualTo(1000L);
+    }
+
+    @Test
+    void print() {
+        String result = formatter.print(1000, Locale.KOREA);
+        assertThat(result).isEqualTo("1,000");
+    }
+}
